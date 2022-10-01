90fe3f509379a4814b0e2e2831ed6a60e6b60595
Stefan Hagen <stefan@hagen.link>
2022-10-01 17:35:15 +0200

    Initial CPSR encoded minimal init

--- sammen/__init__.py
+++ sammen/__init__.py
@@ -1,1 +1,7 @@
-__version__ = '2022.7.24'
+# [[[fill git_describe()]]]
+__version__ = '2022.9.25+parent.4d7d73be'
+# [[[end]]] (checksum: 0fe1fb4dccaae23b681874e6f995ea65)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)
+__all__: list[str] = []
