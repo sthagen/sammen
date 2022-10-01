cfa6de1b421cb63f97b8d66bd3fc16d07cefb496
Stefan Hagen <stefan@hagen.link>
2022-10-01 19:26:48 +0200

    Rewrote implementation tests

--- test/test_sammen.py
+++ test/test_sammen.py
@@ -1,9 +1,9 @@
-import pytest
-
-from sammen.sammen import main
+import sammen.sammen as api
 
 
-@pytest.mark.asyncio
-async def test_main():
-    result = await main(['foo'])
-    assert result == 2
+def test_paths_to_watch():
+    assert api.paths_to_watch({'a': ('b', 'c')}) == ('.',)
+
+
+def test_paths_exist():
+    assert api.paths_exist(('this-should-not-exist',)) == (False, 'this-should-not-exist is no directory')
