43430b4acbfcc7bdf9cf9d6db96f8f482c70a743
Stefan Hagen <stefan@hagen.link>
2022-10-01 19:26:20 +0200

    Added CLI tests

---
+++ test/test_cli.py
@@ -0,0 +1,46 @@
+import pathlib
+
+import pytest
+
+import sammen.cli as cli
+import sammen.sammen as api
+
+TEST_PREFIX = pathlib.Path('test', 'fixtures')
+DEFAULT_DOCUMENTS_PATH = TEST_PREFIX
+TEST_MAKE_MISSING = 'missing-this-file-for-'
+
+
+def test_parse_request(capsys):
+    options = cli.parse_request([])
+    assert options == 0  # type: ignore
+    out, err = capsys.readouterr()
+    assert 'usage: sammen [-h] [--dry-run] [--quiet] [--verbose] families' in out
+    assert not err
+
+
+def test_parse_request_doc_root_option(capsys):
+    options = cli.parse_request([f'{DEFAULT_DOCUMENTS_PATH}', '-q'])
+    assert options.families == [str(DEFAULT_DOCUMENTS_PATH)]  # type: ignore
+    out, err = capsys.readouterr()
+    assert not out
+    assert not err
+
+
+def test_parse_request_pos(capsys):
+    options = cli.parse_request([f'{DEFAULT_DOCUMENTS_PATH}', '--dry-run'])
+    assert options.families == [f'{DEFAULT_DOCUMENTS_PATH}']  # type: ignore
+    out, err = capsys.readouterr()
+    assert not out
+    assert not err
+
+
+def test_parse_request_pos_doc_root_not_present(capsys):
+    with pytest.raises(SystemExit) as err:
+        cli.parse_request([f'{TEST_MAKE_MISSING}{DEFAULT_DOCUMENTS_PATH}', '-q', '-v'])
+    assert err.value.code == 2
+    out, err = capsys.readouterr()
+    assert not out
+    message_part = (
+        f'sammen: error: you cannot be quiet and verbose at the same time'
+    )
+    assert message_part in err
