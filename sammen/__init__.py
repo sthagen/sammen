479c37c5bc3e2a15386e240e849a97de8c3ebdd0
Stefan Hagen <stefan@hagen.link>
2023-10-21 13:55:40 +0200

    Bumped impl version for release 2023.10.21

--- sammen/__init__.py
+++ sammen/__init__.py
@@ -6,8 +6,8 @@ import pathlib
 from typing import no_type_check
 
 # [[[fill git_describe()]]]
-__version__ = '2022.10.2+parent.03bda739'
-# [[[end]]] (checksum: 56721c6ad953105cc6c64a09dc952a5a)
+__version__ = '2023.10.21+parent.65f49136'
+# [[[end]]] (checksum: d84f3327b1ae7bd558038d223e5eaced)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
